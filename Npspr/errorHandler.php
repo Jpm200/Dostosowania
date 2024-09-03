13a23c025193ccff7fadfad36b186576cbd4826b
Jpm200 <127246662+Jpm200@users.noreply.github.com>
2024-09-03 19:35:39 +0200

    Delete Npspr directory

--- Npspr/errorHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-// function ErrorHandler($errno, $errstr, $errfile, $errline) {
-//     header('Location: error.html'); 
-// }
-// function Exception($exception) {
-//     header('Location: error.html');
-// }
-// set_error_handler("ErrorHandler",E_ALL);
-// set_exception_handler("Exception");
--- Npspr/pdf.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include 'conn.php';
-    include 'errorHandler.php';
-$_SESSION['uwagi'] = $_GET['uwagi'];
